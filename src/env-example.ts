25e6ccf3704c5b27776183dd8033496ce11ccb03
Noah Glaser <glaserpower@gmail.com>
2020-11-07 15:53:19 -0800

    committing example for setup instructions

---
+++ src/env-example.ts
@@ -0,0 +1,14 @@
+export default {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
+  server_arduino_url: "",
+  bucket_name: "",
+};
