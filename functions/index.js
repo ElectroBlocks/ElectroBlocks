772c85438f725177c0daddbca01a9faadcc03d66
Noah Glaser <glaserpower@gmail.com>
2020-11-14 23:59:42 -0800

    committing new file

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require("firebase-functions");
 const { sapper } = require("./__sapper__/build/server/server");
 const middleware = sapper.middleware();
+const cors = require("cors")({ origin: true });
 
 exports.ssr = functions.https.onRequest((req, res) => {
-  req.baseUrl = "";
-  middleware(req, res);
+  cors(req, res, () => {
+        req.baseUrl = "";
+        middleware(req, res);
+    });
+
 });
