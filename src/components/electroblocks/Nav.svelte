7a096cca86de38a72451852b3aa29af356ce7020
Noah Glaser <glaserpower@gmail.com>
2020-12-02 23:23:19 -0800

    fixed confirm new file

--- src/components/electroblocks/Nav.svelte
+++ src/components/electroblocks/Nav.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-
   export let segment: string;
-  import authStore from '../../stores/auth.store';
-  import projectStore from '../../stores/project.store';
-  import { isPathOnHomePage } from '../../helpers/is-path-on-homepage';
-  import { fade } from 'svelte/transition'
+  import authStore from "../../stores/auth.store";
+  import projectStore from "../../stores/project.store";
+  import { isPathOnHomePage } from "../../helpers/is-path-on-homepage";
+  import { fade } from "svelte/transition";
   import { stores, goto } from "@sapper/app";
-  import { logout } from '../../firebase/auth';
-  import { loadNewProjectFile } from '../../helpers/open-project-file';
-  import { arduinoLoopBlockShowLoopForeverText, arduinoLoopBlockShowNumberOfTimesThroughLoop } from '../../core/blockly/helpers/arduino_loop_block.helper';
-  import { resetWorkspace } from '../../core/blockly/helpers/workspace.helper';
-  import { saveProject } from '../../firebase/db';
-  import { wait } from '../../helpers/wait';
-import { onConfirm, onErrorMessage } from '../../help/alerts';
+  import { logout } from "../../firebase/auth";
+  import { loadNewProjectFile } from "../../helpers/open-project-file";
+  import {
+    arduinoLoopBlockShowLoopForeverText,
+    arduinoLoopBlockShowNumberOfTimesThroughLoop,
+  } from "../../core/blockly/helpers/arduino_loop_block.helper";
+  import { resetWorkspace } from "../../core/blockly/helpers/workspace.helper";
+  import { saveProject } from "../../firebase/db";
+  import { wait } from "../../helpers/wait";
+  import { onConfirm, onErrorMessage } from "../../help/alerts";
+
   let isOpeningFile = false;
   let fileUpload;
   let canSave = true;
@@ -20,46 +23,47 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
 
   const { page } = stores();
 
-  let params = '';
-  projectStore.subscribe(p => {
+  let params = "";
+  projectStore.subscribe((p) => {
     if (p.projectId) {
-        params = `?projectid=${p.projectId}`;
+      params = `?projectid=${p.projectId}`;
     } else {
-      params = '';
+      params = "";
     }
-  })
+  });
 
   async function onNewFileAuth() {
-    
-      if (!$projectStore.project) {
-        onNewFileNoAuth();
-        return;
-      }
+    if (!$projectStore.project) {
+      onNewFileNoAuth();
+      return;
+    }
 
-      const confirmNewFile = await onConfirm('We are about to save your current project and create a new one? Would you like to continue?');
+    const confirmNewFile = await onConfirm(
+      "We are about to save your current project and create a new one? Would you like to continue?"
+    );
 
-      if (!confirmNewFile) {
-        return;
-      }
+    if (!confirmNewFile) {
+      return;
+    }
     try {
       await saveProject($projectStore.project, $projectStore.projectId);
       projectStore.set({ projectId: null, project: null });
       await goto("/");
       resetWorkspace();
-    } catch(e) {
-       onErrorMessage("Error saving your project please try agian.", e);
+    } catch (e) {
+      onErrorMessage("Error saving your project please try agian.", e);
     }
   }
 
   async function onNewFileNoAuth() {
-    const confirmNewFile = onConfirm('You are creating a new file, which will delete your work.  Would you like to continue?');
-
+    const confirmNewFile = await onConfirm(
+      "You are creating a new file, which will delete your work.  Would you like to continue?"
+    );
     if (!confirmNewFile) {
       return;
     }
 
     resetWorkspace();
-    
   }
 
   async function onSaveClick() {
@@ -75,31 +79,30 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
       await wait(1500);
       canSave = true;
       showSaveSuccess = false;
-    } catch(e) {
-       onErrorMessage("Error saving your project please try agian.", e);
+    } catch (e) {
+      onErrorMessage("Error saving your project please try agian.", e);
     }
   }
-  
 
   async function onSignOut() {
     try {
-        await logout();
-    } catch(e) {
+      await logout();
+    } catch (e) {
       onErrorMessage("Please try again in 5 minutes", e);
     }
   }
 
- async function openFile(e) {
+  async function openFile(e) {
     isOpeningFile = true;
     const file = fileUpload.files[0];
     if (!file) {
-      return
+      return;
     }
 
     try {
-        await loadNewProjectFile(file);
+      await loadNewProjectFile(file);
     } catch (e) {
-        onErrorMessage("Please make sure you uploaded a valid file.", e);
+      onErrorMessage("Please make sure you uploaded a valid file.", e);
     }
 
     isOpeningFile = false;
@@ -108,8 +111,7 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
     } else {
       arduinoLoopBlockShowLoopForeverText();
     }
-}
-  
+  }
 </script>
 
 <style>
@@ -135,7 +137,8 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
   }
 
   nav a,
-  nav span, label {
+  nav span,
+  label {
     float: left;
     width: 10%;
     text-align: center;
@@ -145,8 +148,10 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
     font-size: 30px;
     cursor: pointer;
   }
-  nav.small a, nav.small span, nav.small label {
-    width: 11.11111%
+  nav.small a,
+  nav.small span,
+  nav.small label {
+    width: 11.11111%;
   }
   #saved {
     position: absolute;
@@ -159,40 +164,43 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
     transform: translateX(-50%);
     z-index: 21;
     color: #fff;
-}
+  }
 </style>
 
-<nav class:small={!$authStore.isLoggedIn} >
-  {#if $authStore.isLoggedIn }
-  <a href="/{params}" class:active={isPathOnHomePage($page.path)}>
-    <i class="fa fa-home" />
-  </a>
+<nav class:small={!$authStore.isLoggedIn}>
+  {#if $authStore.isLoggedIn}
+    <a href="/{params}" class:active={isPathOnHomePage($page.path)}>
+      <i class="fa fa-home" />
+    </a>
 
-  <a href="/code{params}" class:active={$page.path.includes('code')}>
-    <i class="fa fa-code" />
-  </a>
-  <a href="/arduino{params}" class:active={$page.path.includes('arduino')}>
-    <i class="fa fa-microchip" />
-  </a>
-  <a href="/lessons" class:active={segment === 'lessons'}>
-    <i class="fa fa-book" />
-  </a>
-  <a href="/open" class:active={segment === 'open'}>
-    <i class="fa " class:fa-folder-open-o={segment !== 'open'} class:fa-folder-open={segment === 'open'} />
-  </a>
-  <span on:click={onNewFileAuth} >
-    <i class="fa fa-file-o"></i>
-  </span>
-  <span on:click={onSaveClick} ><i class="fa fa-floppy-o"></i></span>
-  <a href="/project-settings" class:active={$page.path.includes('project-settings')}>
-      <i class="fa fa-wrench" aria-hidden="true"></i>
+    <a href="/code{params}" class:active={$page.path.includes('code')}>
+      <i class="fa fa-code" />
+    </a>
+    <a href="/arduino{params}" class:active={$page.path.includes('arduino')}>
+      <i class="fa fa-microchip" />
+    </a>
+    <a href="/lessons" class:active={segment === 'lessons'}>
+      <i class="fa fa-book" />
+    </a>
+    <a href="/open" class:active={segment === 'open'}>
+      <i
+        class="fa "
+        class:fa-folder-open-o={segment !== 'open'}
+        class:fa-folder-open={segment === 'open'} />
+    </a>
+    <span on:click={onNewFileAuth}> <i class="fa fa-file-o" /> </span>
+    <span on:click={onSaveClick}><i class="fa fa-floppy-o" /></span>
+    <a
+      href="/project-settings"
+      class:active={$page.path.includes('project-settings')}>
+      <i class="fa fa-wrench" aria-hidden="true" />
     </a>
     <a href="/settings" class:active={segment === 'settings'}>
-    <i class="fa fa-gears" />
-  </a>
-   <span on:click={onSignOut}>
-    <i class="fa fa-sign-out" title="Sign Out" aria-hidden="true"></i>
-  </span>
+      <i class="fa fa-gears" />
+    </a>
+    <span on:click={onSignOut}>
+      <i class="fa fa-sign-out" title="Sign Out" aria-hidden="true" />
+    </span>
   {/if}
 
   {#if !$authStore.isLoggedIn}
@@ -200,35 +208,41 @@ import { onConfirm, onErrorMessage } from '../../help/alerts';
       <i class="fa fa-home" />
     </a>
 
-  <a href="/code" class:active={$page.path.includes('code')}>
-    <i class="fa fa-code" />
-  </a>
-  <a href="/arduino" class:active={$page.path.includes('arduino')}>
-    <i class="fa fa-microchip" />
-  </a>
-  <a href="/lessons" class:active={segment === 'lessons'}>
-    <i class="fa fa-book" />
-  </a>
-  <label class:active={segment === 'open'}>
-    <i class="fa " class:fa-folder-open-o={!isOpeningFile} class:fa-folder-open={isOpeningFile} />
-    <input on:change={openFile} type="file"  accept="text/xml" style="display:none" bind:this={fileUpload}/>
-  </label>
-  <span on:click={onNewFileNoAuth} class="active">
-    <i class="fa fa-file-o"></i>
-  </span>
-  <a href="/download" class:active={segment === 'download'}>
-    <i class="fa fa-download" />
-  </a>
-  <a href="/settings" class:active={segment === 'settings'}>
-    <i class="fa fa-gears" />
-  </a>
-   <a href="/login" class:active={segment === 'login'}>
-    <i class="fa fa-sign-in" />
-  </a>
-  {/if}  
+    <a href="/code" class:active={$page.path.includes('code')}>
+      <i class="fa fa-code" />
+    </a>
+    <a href="/arduino" class:active={$page.path.includes('arduino')}>
+      <i class="fa fa-microchip" />
+    </a>
+    <a href="/lessons" class:active={segment === 'lessons'}>
+      <i class="fa fa-book" />
+    </a>
+    <label class:active={segment === 'open'}>
+      <i
+        class="fa "
+        class:fa-folder-open-o={!isOpeningFile}
+        class:fa-folder-open={isOpeningFile} />
+      <input
+        on:change={openFile}
+        type="file"
+        accept="text/xml"
+        style="display:none"
+        bind:this={fileUpload} />
+    </label>
+    <span on:click={onNewFileNoAuth} class="active">
+      <i class="fa fa-file-o" />
+    </span>
+    <a href="/download" class:active={segment === 'download'}>
+      <i class="fa fa-download" />
+    </a>
+    <a href="/settings" class:active={segment === 'settings'}>
+      <i class="fa fa-gears" />
+    </a>
+    <a href="/login" class:active={segment === 'login'}>
+      <i class="fa fa-sign-in" />
+    </a>
+  {/if}
 </nav>
 {#if showSaveSuccess}
-<p transition:fade id="saved">
-  project saved
-</p>
+  <p transition:fade id="saved">project saved</p>
 {/if}
