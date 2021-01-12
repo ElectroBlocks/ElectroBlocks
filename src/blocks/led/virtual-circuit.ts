f3206ff3482dc45269da07ba6a46df20e5b0ed56
Noah Glaser <glaserpower@gmail.com>
2021-01-11 21:48:29 -0800

    fixed led color bug

--- src/blocks/led/virtual-circuit.ts
+++ src/blocks/led/virtual-circuit.ts
@@ -39,12 +39,12 @@ export const ledCreate: CreateCompenentHook<LedState> = (
   }
 
   ledEl.data("pin-number", state.pin);
-
   ledEl
     .find(`#radial-gradient-${state.pin} stop`)
     .toArray()
-    .find((stop) => stop.attr("offset") == 1)
+    .find((stopEl) => stopEl.attr("offset") == 1)
     .attr("stop-color", ledColor);
+
   ledEl.data("color", ledColor);
   const pinText = ledEl.findOne("#PIN_NUMBER") as Text;
   pinText.node.innerHTML = state.pin;
