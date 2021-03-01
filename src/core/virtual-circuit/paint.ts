4b90968b7262b499b5d7714aeccf4b9bb6719754
Noah Glaser <glaserpower@gmail.com>
2021-02-28 23:50:04 -0800

    got minus point working

--- src/core/virtual-circuit/paint.ts
+++ src/core/virtual-circuit/paint.ts
@@ -67,7 +67,8 @@ const findOrCreateMicroController = (draw: Svg, board: MicroController) => {
   const zoomWidth = draw.width() / arduino.width();
   const minusAmount = draw.height() - 300 > 200 ? 250 : 150;
   const zoomHeight = (draw.height() - minusAmount) / arduino.height();
-  (draw as any).zoom(zoomHeight < zoomWidth ? zoomHeight : zoomWidth); // ZOOM MUST GO FIRST TO GET THE RIGHT X Y VALUES IN POSITIONING.
+  (draw as any).zoom((zoomHeight < zoomWidth ? zoomHeight : zoomWidth) - 0.1); // ZOOM MUST GO FIRST TO GET THE RIGHT X Y VALUES IN POSITIONING.
+  // Minus .1 is so that lcd screen and other things fit in.
   arduino.y(draw.viewbox().y2 - arduino.height() + 30);
   arduino.x(-30);
 
