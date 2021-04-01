d33f0db3794a44a66a2a415337aec18683d40782
Noah Glaser <glaserpower@gmail.com>
2021-04-01 14:59:20 -0700

    Got the arduino code working

--- src/blocks/digit4display/generators.ts
+++ src/blocks/digit4display/generators.ts
@@ -1,43 +1,45 @@
 import Blockly, { Block } from "blockly";
 
 Blockly["Arduino"]["digital_display_setup"] = function (block: Block) {
-  var dioPin = block.getFieldValue("DIO_PIN");
-  var clkPin = block.getFieldValue("CLK_PIN");
+  let dioPin = block.getFieldValue("DIO_PIN");
+  let clkPin = block.getFieldValue("CLK_PIN");
 
   Blockly["Arduino"].libraries_["define_digital_display"] = `
+#include "SevenSegmentTM1637.h"
+String digitalDisplayPrint123 = "";
 const byte PIN_CLK = ${dioPin};   // define CLK pin (any digital pin)
 const byte PIN_DIO = ${clkPin};   // define DIO pin (any digital pin)
 SevenSegmentTM1637    digitalDisplay(PIN_CLK, PIN_DIO);
 `;
 
-  Blockly["Arduino"].setupCode_["digital_display_setup"] = `
-
-    digitalDisplay.begin();
+  Blockly["Arduino"].setupCode_[
+    "digital_display_setup"
+  ] = `  digitalDisplay.begin();
     digitalDisplay.setBacklight(100);
-
 `;
 
-  // TODO: Assemble JavaScript into code variable.
-  var code = "";
+  let code = "";
   return code;
 };
 
 Blockly["Arduino"]["digital_display_set"] = function (block) {
-  var text = Blockly["Arduino"].valueToCode(
+  let text = Blockly["Arduino"].valueToCode(
     block,
     "TEXT",
     Blockly["Arduino"].ORDER_ATOMIC
   );
-  var colonOn = block.getFieldValue("COLON") == "TRUE";
+  let colonOn = block.getFieldValue("COLON") == "TRUE";
 
-  // display.clear();
-  // display.setColonOn(true);
-  // display.print("1200");
+  // This has to 4 in order to get the colons to work
 
-  // TODO: Assemble JavaScript into code variable.
-  var code = `  display.clear();
-  display.setColonOn(${colonOn});
-  display.print(${text});
+  let code = `
+  digitalDisplayPrint123 = ${text};
+  for (int i = digitalDisplayPrint123.length(); i < 4; i += 1) {
+    digitalDisplayPrint123 += " ";
+  }
+  digitalDisplay.clear();
+  digitalDisplay.setColonOn(${colonOn});
+  digitalDisplay.print(${text});
 `;
   return code;
 };
