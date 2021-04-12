30da7808cec3d6395b3cf47ad5e13d39322bdc9b
Noah Glaser <glaserpower@gmail.com>
2021-04-11 21:24:41 -0700

    fixed bug with virtual circuit

--- src/blocks/joystick/virtual-circuit.ts
+++ src/blocks/joystick/virtual-circuit.ts
@@ -122,7 +122,7 @@ export const updateJoyStick: SyncComponent = (
 
   if (state.engaged) {
     const moveBy = 20;
-    const adjustedDegree = state.degree + 90;
+    const adjustedDegree = state.degree * -1 + 180;
     const radRatio = Math.PI / 180;
     movingPart.cx(
       containingPart.cx() + Math.cos(adjustedDegree * radRatio) * moveBy
