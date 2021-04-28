654fa24a7c2bb71891c8460d9a832f6c184439a6
Noah Glaser <glaserpower@gmail.com>
2021-04-27 23:19:24 -0700

    got the svg positioning working

--- src/microcontrollers/arduino_mega/virtual-circuit.ts
+++ src/microcontrollers/arduino_mega/virtual-circuit.ts
@@ -1,11 +1,8 @@
 import { AfterCreateBoard } from "../../core/microcontroller/microcontroller.virtual-circuit-interfaces";
 
 export const afterCreateMega: AfterCreateBoard = (arduino, draw) => {
-  const zoomWidth = draw.width() / arduino.width();
-  const minusAmount = draw.height() - 300 > 200 ? 250 : 150;
-  const zoomHeight = (draw.height() - minusAmount) / arduino.height();
-  (draw as any).zoom((zoomHeight < zoomWidth ? zoomHeight : zoomWidth) - 0.1); // ZOOM MUST GO FIRST TO GET THE RIGHT X Y VALUES IN POSITIONING.
-  // Minus .1 is so that lcd screen and other things fit in.
-  arduino.y(draw.viewbox().y2 - arduino.height() + 30);
-  arduino.x(-30);
+  draw.viewbox(arduino.bbox());
+  (draw as any).zoom((draw as any).zoom() - 0.15);
+  // move it down 150 px
+  arduino.dy(150);
 };
--- src/microcontrollers/arduino_uno/virtual-circuit.ts
+++ src/microcontrollers/arduino_uno/virtual-circuit.ts
@@ -1,8 +1,8 @@
-import { Matrix } from "@svgdotjs/svg.js";
 import { AfterCreateBoard } from "../../core/microcontroller/microcontroller.virtual-circuit-interfaces";
 
 export const afterCreateUno: AfterCreateBoard = (arduino, draw) => {
   draw.viewbox(arduino.bbox());
   (draw as any).zoom((draw as any).zoom() - 0.15);
-  arduino.y((draw as any).zoom() * 220);
+  // move it down 150 px
+  arduino.dy(150);
 };
