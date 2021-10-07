fc0071626f4dfa21ead8f16341af87bedc07cdc0
phptuts <glaserpower@gmail.com>
2021-10-07 14:47:07 -0700

    Fixed default color values in generator

--- src/blocks/rgbled/generators.ts
+++ src/blocks/rgbled/generators.ts
@@ -1,46 +1,51 @@
-import Blockly from "blockly";
-import type { Block } from "blockly";
+import Blockly from 'blockly';
+import type { Block } from 'blockly';
+import _ from 'lodash';
 
-Blockly["Arduino"]["rgb_led_setup"] = function (block: Block) {
-  const redPin = block.getFieldValue("PIN_RED");
-  const greenPin = block.getFieldValue("PIN_GREEN");
-  const bluePin = block.getFieldValue("PIN_BLUE");
+Blockly['Arduino']['rgb_led_setup'] = function (block: Block) {
+  const redPin = block.getFieldValue('PIN_RED');
+  const greenPin = block.getFieldValue('PIN_GREEN');
+  const bluePin = block.getFieldValue('PIN_BLUE');
 
-  Blockly["Arduino"].setupCode_[
-    "led_pin_" + redPin
+  Blockly['Arduino'].setupCode_[
+    'led_pin_' + redPin
   ] = `\tpinMode(RED_PIN, OUTPUT); \n`;
-  Blockly["Arduino"].setupCode_[
-    "led_pin_" + greenPin
+  Blockly['Arduino'].setupCode_[
+    'led_pin_' + greenPin
   ] = `\tpinMode(GREEN_PIN, OUTPUT); \n`;
-  Blockly["Arduino"].setupCode_[
-    "led_pin_" + bluePin
+  Blockly['Arduino'].setupCode_[
+    'led_pin_' + bluePin
   ] = `\tpinMode(BLUE_PIN, OUTPUT); \n`;
 
-  Blockly["Arduino"].libraries_["color_pin_blue"] =
-    "int BLUE_PIN = " + bluePin + ";\n";
+  Blockly['Arduino'].libraries_['color_pin_blue'] =
+    'int BLUE_PIN = ' + bluePin + ';\n';
 
-  Blockly["Arduino"].libraries_["color_pin_red"] =
-    "int RED_PIN = " + redPin + ";\n";
+  Blockly['Arduino'].libraries_['color_pin_red'] =
+    'int RED_PIN = ' + redPin + ';\n';
 
-  Blockly["Arduino"].libraries_["color_pin_green"] =
-    "int GREEN_PIN = " + greenPin + ";\n";
+  Blockly['Arduino'].libraries_['color_pin_green'] =
+    'int GREEN_PIN = ' + greenPin + ';\n';
 
-  Blockly["Arduino"].functionNames_["_setLedColor"] =
-    "void setLedColor(RGB color) {\n" +
-    "\tanalogWrite(GREEN_PIN, color.green); \n" +
-    "\tanalogWrite(BLUE_PIN, color.blue); \n" +
-    "\tanalogWrite(RED_PIN, color.red); \n" +
-    "}\n";
+  Blockly['Arduino'].functionNames_['_setLedColor'] =
+    'void setLedColor(RGB color) {\n' +
+    '\tanalogWrite(GREEN_PIN, color.green); \n' +
+    '\tanalogWrite(BLUE_PIN, color.blue); \n' +
+    '\tanalogWrite(RED_PIN, color.red); \n' +
+    '}\n';
 
-  return "";
+  return '';
 };
 
-Blockly["Arduino"]["set_color_led"] = function (block: Block) {
-  const color = Blockly["Arduino"].valueToCode(
+Blockly['Arduino']['set_color_led'] = function (block: Block) {
+  let color = Blockly['Arduino'].valueToCode(
     block,
-    "COLOUR",
-    Blockly["Arduino"].ORDER_ATOMIC
+    'COLOUR',
+    Blockly['Arduino'].ORDER_ATOMIC
   );
 
-  return "setLedColor(" + color + ");\n";
+  if (_.isEmpty(color)) {
+    color = '{ 0, 0, 0 }';
+  }
+
+  return 'setLedColor(' + color + ');\n';
 };
