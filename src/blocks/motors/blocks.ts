de0d781278531dccd39af6693962519d9e9f0722
vasu737et <140981963+vasu737et@users.noreply.github.com>
2024-05-30 15:11:44 +0530

    Update blocks.ts

--- src/blocks/motors/blocks.ts
+++ src/blocks/motors/blocks.ts
@@ -28,8 +28,8 @@ Blockly.defineBlocksWithJsonArray([
         type: "field_dropdown",
         name: "DIRECTION",
         options: [
-          ["Forward", "FORWARD"],
-          ["Backward", "BACKWARD"],
+          ["Clockwise", "Clockwise"],
+          ["AntiClockwise", "Anticlockwise"],
         ],
       },
       {
