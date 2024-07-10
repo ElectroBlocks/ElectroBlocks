231b7efc6e721318d60e54e4324ae8423e32a2ed
Noah-Glaser <noah.glaser@docusign.com>
2024-07-09 22:06:06 -0700

    fixed pins bug

--- src/blocks/digit4display/component-state-to-id.ts
+++ src/blocks/digit4display/component-state-to-id.ts
@@ -1,5 +1,5 @@
 import type { DigitilDisplayState } from "./state";
 
 export const getDigitalDisplayId = (state: DigitilDisplayState) => {
-  return `${state.type}-${state.componentType}`;
+  return `${state.type}-${state.componentType}-${state.pins.join(",")}`;
 };
