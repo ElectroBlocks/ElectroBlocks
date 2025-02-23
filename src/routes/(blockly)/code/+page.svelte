863491659b14bd6065d7cc8559a040b64dcc66e8
Anushiya04 <anushiyamuthuvel@gmail.com>
2025-02-23 14:59:12 +0530

    Fixed_toolbox_visibility

--- src/routes/(blockly)/code/+page.svelte
+++ src/routes/(blockly)/code/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import { onMount, afterUpdate } from "svelte";
+  import { get } from "svelte/store";
   import codeStore from "../../../stores/code.store";
+  import boardStore from "../../../stores/board.store"; // Import board selection store
 
   import hljs from 'highlight.js/lib/core';
   import arduinoLang from 'highlight.js/lib/languages/arduino';
@@ -8,23 +10,31 @@
   import 'highlight.js/styles/a11y-light.css';
 
   import { tooltip } from "@svelte-plugins/tooltips";
-  import { get } from "svelte/store";
 
   let code = "";
   let loaded = false;
   let fontSize = 14;
   let hasCopiedCode = false;
-  let hiddenCategories = [];
+  let isBluetoothEnabled = true; // Default: enabled
 
-  onMount(() => {
+  onMount(async () => {
     hljs.registerLanguage('arduino', arduinoLang);
-    codeStore.subscribe((codeInfo) => {
+    
+    // Subscribe to code updates
+    codeStore.subscribe(async (codeInfo) => {
       try {
         code = hljs.highlight(codeInfo.code, { language: 'arduino' }).value;
-        hiddenCategories = codeInfo.hiddenCategories || [];
-        console.log("Hidden Categories Updated:", hiddenCategories);
       } catch (e) {
         console.log(e);
+      }
+    });
+
+    // Subscribe to board selection changes
+    boardStore.subscribe(board => {
+      if (board === "Arduino Mega") {
+        isBluetoothEnabled = false; // Disable Bluetooth for Mega
+      } else if (board === "Arduino Uno") {
+        isBluetoothEnabled = true; // Enable Bluetooth for Uno
       }
     });
 
@@ -36,7 +46,7 @@
       try {
         hljs.highlightAll();
       } catch (error) {
-        console.log(error, 'error')
+        console.log(error, 'error');
       }
     }
   });
@@ -53,49 +63,44 @@
     navigator.clipboard.writeText(get(codeStore).code);
     hasCopiedCode = true;
   }
+
+  const navTooltipStyleCodeSmallMarginBottom = {
+    position: "bottom",
+    align: "center",
+    animation: "slide",
+    theme: "code-small-margin",
+  };
+  const navTooltipStyleSmallMargin = {
+    position: "bottom",
+    align: "center",
+    animation: "slide",
+    theme: "code-large-margin",
+  };
 </script>
 
 <div class="row">
   <div class="col">
     {#if !hasCopiedCode}
-      <i use:tooltip title="Copy Code" on:click={copy} class="fa fa-clipboard" aria-hidden="true" />
+      <i use:tooltip={navTooltipStyleSmallMargin} title="Copy Code" on:click={copy} class="fa fa-clipboard" aria-hidden="true"></i>
     {:else}
-      <i use:tooltip title="Copied" on:mouseleave={() => hasCopiedCode = false} on:click={copy} class="fa fa-clipboard" aria-hidden="true" />
+      <i use:tooltip={navTooltipStyleSmallMargin} title="Copied" on:mouseleave={() => hasCopiedCode = false} on:click={copy} class="fa fa-clipboard" aria-hidden="true"></i>
     {/if}
-    <i use:tooltip on:click={zoomOut} title="Zoom Out" class="fa fa-search-minus float-end me-4" aria-hidden="true" />
-    <i use:tooltip on:click={zoomIn} title="Zoom In" class="fa fa-search-plus float-end" aria-hidden="true" />
+    
+    <i use:tooltip={navTooltipStyleCodeSmallMarginBottom} on:click={zoomOut} title="Zoom Out" class="fa fa-search-minus float-end me-4" aria-hidden="true"></i>
+    
+    <i use:tooltip={navTooltipStyleSmallMargin} on:click={zoomIn} title="Zoom In" class="fa fa-search-plus float-end" aria-hidden="true"></i>
   </div>
 </div>
+
+<!-- Display Bluetooth status -->
+<p class="bluetooth-status">
+  Bluetooth: <strong>{isBluetoothEnabled ? "Enabled" : "Disabled"}</strong>
+</p>
 
 <pre style="font-size: {fontSize}px">
   <code class="language-arduino">{@html code}</code>
 </pre>
 
-<svelte:head>
-  <title>ElectroBlocks - Code</title>
-</svelte:head>
-<div class="row">
-  <div class="col">
-    {#if !hasCopiedCode}
-    <i use:tooltip={navTooltipStyleSmallMargin} title="Copy Code" on:click={copy}  class="fa fa-clipboard" aria-hidden="true" />
-    {:else}
-    <i use:tooltip={navTooltipStyleSmallMargin} title="Copied" on:mouseleave={() => hasCopiedCode = false} on:click={copy}  class="fa fa-clipboard" aria-hidden="true" />
-    {/if}
-    <i       
-      use:tooltip={navTooltipStyleCodeSmallMarginBottom}
-      on:click={zoomOut} 
-      title="Zoom Out" 
-      class="fa fa-search-minus float-end me-4"
-      aria-hidden="true" />
-    <i use:tooltip={navTooltipStyleSmallMargin} 
-      on:click={zoomIn} title="Zoom In"  
-      class="fa fa-search-plus float-end" 
-      aria-hidden="true" />
-  </div>
-</div>
-<pre style="font-size: {fontSize}px">
-  <code class="{get(codeStore).boardType === 'python' ? 'language-python' : 'language-arduino'}">{@html code}</code>
-  </pre>
 <svelte:head>
   <title>ElectroBlocks - Code</title>
 </svelte:head>
@@ -122,5 +127,10 @@
   }
   :global(.tooltip.code-large-margin) {
     margin-top: 30px;
+  }
+  .bluetooth-status {
+    font-size: 16px;
+    margin-left: 20px;
+    color: #333;
   }
 </style>
