6350a73e16f04791654abd6db45f0c27d2885b48
Noah-Glaser <noah.glaser@docusign.com>
2025-03-12 22:44:04 -0700

    testing resized gifs and lazy loading

--- src/lessons/lessons.ts
+++ src/lessons/lessons.ts
@@ -16,25 +16,25 @@ export const lessons: LessonContainer[] = [
     lessons: [
       {
         file: "blink.xml",
-        image: "/example-projects/blink.gif",
+        image: "/example-projects/resized/blink.gif",
         title: "Blink",
         levelImage: "/example-projects/easy.png",
       },
       {
         file: "traffic_lights.xml",
-        image: "/example-projects/traffic_lights.gif",
+        image: "/example-projects/resized/traffic_lights.gif",
         title: "Traffic Lights",
         levelImage: "/example-projects/easy.png",
       },
       {
         file: "automatic_parking_gate.xml",
-        image: "/example-projects/automatic_parking_gate.gif",
+        image: "/example-projects/resized/automatic_parking_gate.gif",
         title: "Automatic Parking Gate",
         levelImage: "/example-projects/medium.png",
       },
       {
         file: "car_parking_system.xml",
-        image: "/example-projects/car_parking_system.gif",
+        image: "/example-projects/resized/car_parking_system.gif",
         title: "Car Parking System",
         levelImage: "/example-projects/medium.png",
       },
@@ -45,7 +45,7 @@ export const lessons: LessonContainer[] = [
     lessons: [
       {
         file: "rgb_led_pushbutton.xml",
-        image: "/example-projects/rgb_led_pushbutton.gif",
+        image: "/example-projects/resized/rgb_led_pushbutton.gif",
         title: "RGB LED with button",
         levelImage: "/example-projects/medium.png",
       },
@@ -56,13 +56,13 @@ export const lessons: LessonContainer[] = [
     lessons: [
       {
         file: "intruder_sensor.xml",
-        image: "/example-projects/intruder_sensor.gif",
+        image: "/example-projects/resized/intruder_sensor.gif",
         title: "Intruder Sensor",
         levelImage: "/example-projects/medium.png",
       },
       {
         file: "solar_tracking_system.xml",
-        image: "/example-projects/solar_tracking_system.gif",
+        image: "/example-projects/resized/solar_tracking_system.gif",
         title: "Solar Tracking System",
         levelImage: "/example-projects/medium.png",
       },
@@ -84,7 +84,7 @@ export const lessons: LessonContainer[] = [
     lessons: [
       {
         file: "lcd_display_text.xml",
-        image: "/example-projects/lcd_display_text.gif",
+        image: "/example-projects/resized/lcd_display_text.gif",
         title: "Display Text",
         levelImage: "/example-projects/easy.png",
       },
@@ -107,37 +107,37 @@ export const lessons: LessonContainer[] = [
       },
       {
         file: "christmas_lights.xml",
-        image: "/example-projects/christmas_lights.gif",
+        image: "/example-projects/resized/christmas_lights.gif",
         title: "Christmas Lights",
         levelImage: "/example-projects/medium.png",
       },
       {
         file: "snake.xml",
-        image: "/example-projects/snake.gif",
+        image: "/example-projects/resized/snake.gif",
         title: "Snake Pattern",
         levelImage: "/example-projects/hard.png",
       },
       {
         file: "rainbow.xml",
-        image: "/example-projects/rainbow.gif",
+        image: "/example-projects/resized/rainbow.gif",
         title: "Rainbow Lights",
         levelImage: "/example-projects/hard.png",
       },
       {
         file: "rainbow_rolling.xml",
-        image: "/example-projects/rainbow_streaming.gif",
+        image: "/example-projects/resized/rainbow_streaming.gif",
         title: "Rainbow Lights Streaming",
         levelImage: "/example-projects/hard.png",
       },
       {
         file: "sonar_rgbleds.xml",
-        image: "/example-projects/sonar_rgbleds.gif",
+        image: "/example-projects/resized/sonar_rgbleds.gif",
         title: "Motion Sensor with RGB Leds",
         levelImage: "/example-projects/medium.png",
       },
       {
         file: "sonar_rgbleds_spin.xml",
-        image: "/example-projects/sonar_rgbleds_spin.gif",
+        image: "/example-projects/resized/sonar_rgbleds_spin.gif",
         title: "Motion Sensor with Spinning Lights",
         levelImage: "/example-projects/hard.png",
       },
@@ -154,19 +154,19 @@ export const lessons: LessonContainer[] = [
       },
       {
         file: "ledmatrix_blink.xml",
-        image: "/example-projects/ledmatrix_blink.gif",
+        image: "/example-projects/resized/ledmatrix_blink.gif",
         title: "Wink",
         levelImage: "/example-projects/easy.png",
       },
       {
         file: "ledmatrix_loop.xml",
-        image: "/example-projects/ledmatrix_loop.gif",
+        image: "/example-projects/resized/ledmatrix_loop.gif",
         title: "Led Matrix Loop",
         levelImage: "/example-projects/medium.png",
       },
       {
         file: "ledmatrix_blink.xml",
-        image: "/example-projects/ledmatrix_blink.gif",
+        image: "/example-projects/resized/ledmatrix_blink.gif",
         title: "Wink",
         levelImage: "/example-projects/easy.png",
       },
