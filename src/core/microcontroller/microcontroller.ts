20425711ee7e412ee1c83110d048d1d6e7a9a094
Noah-Glaser <noah.glaser@docusign.com>
2025-03-26 22:52:48 -0700

    Got toolbox working and reloading

--- src/core/microcontroller/microcontroller.ts
+++ src/core/microcontroller/microcontroller.ts
@@ -1,5 +1,3 @@
-import type { ARDUINO_PINS } from "./selectBoard";
-
 export enum MicroControllerType {
   ARDUINO_UNO = "uno",
   ARDUINO_MEGA = "mega",
@@ -9,6 +7,11 @@ export interface BreadBoardArea {
   holes: number[];
   taken: boolean;
   isDown: boolean;
+}
+
+export enum SUPPORTED_LANGUAGES {
+  PYTHON = "Python",
+  C = "C",
 }
 
 export interface PinConnection {
