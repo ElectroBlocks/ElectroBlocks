adbc3120c3ec902c084763ee64e4d307995b9f2e
Anushiya04 <anushiyamuthuvel@gmail.com>
2025-04-28 01:53:09 +0530

    python_rgbled

--- src/blocks/rgbled/generators.ts
+++ src/blocks/rgbled/generators.ts
@@ -3,16 +3,29 @@ import type { Block } from "blockly";
 import _ from "lodash";
 import { hexToRgb, rgbToColorStruct } from "../../core/blockly/helpers/color.helper";
 
-// =============== ARDUINO GENERATOR =============== //
+if (!Blockly.Arduino) {
+  Blockly.Arduino = {};
+}
+Blockly.Arduino.libraries_ = Blockly.Arduino.libraries_ || {};
+Blockly.Arduino.variables_ = Blockly.Arduino.variables_ || {};
+Blockly.Arduino.setupCode_ = Blockly.Arduino.setupCode_ || {};
+Blockly.Arduino.functions_ = Blockly.Arduino.functions_ || {};
+Blockly.Arduino.functionNames_ = Blockly.Arduino.functionNames_ || {};
+
+if (!Blockly.Python) {
+  Blockly.Python = {};
+}
+Blockly.Python.imports_ = Blockly.Python.imports_ || {};
+Blockly.Python.variables_ = Blockly.Python.variables_ || {};
+Blockly.Python.setupCode_ = Blockly.Python.setupCode_ || {};
+Blockly.Python.functions_ = Blockly.Python.functions_ || {};
+
+// =============== ARDUINO GENERATOR ===============
 
 Blockly["Arduino"]["rgb_led_setup"] = function (block: Block) {
   const redPin1 = block.getFieldValue("PIN_RED_1");
   const greenPin1 = block.getFieldValue("PIN_GREEN_1");
   const bluePin1 = block.getFieldValue("PIN_BLUE_1");
-
-  const redPin2 = block.getFieldValue("PIN_RED_2");
-  const greenPin2 = block.getFieldValue("PIN_GREEN_2");
-  const bluePin2 = block.getFieldValue("PIN_BLUE_2");
 
   const numComponents = block.getFieldValue("NUMBER_OF_COMPONENTS");
 
@@ -28,19 +41,23 @@ struct RGB {
   Blockly["Arduino"].variables_["BLUE_PIN_1"] = `int BLUE_PIN_1 = ${bluePin1};`;
 
   Blockly["Arduino"].setupCode_["setup_led1"] = `
-  pinMode(RED_PIN_1, OUTPUT);
-  pinMode(GREEN_PIN_1, OUTPUT);
-  pinMode(BLUE_PIN_1, OUTPUT);`;
+pinMode(RED_PIN_1, OUTPUT);
+pinMode(GREEN_PIN_1, OUTPUT);
+pinMode(BLUE_PIN_1, OUTPUT);`;
 
   if (numComponents === "2") {
+    const redPin2 = block.getFieldValue("PIN_RED_2");
+    const greenPin2 = block.getFieldValue("PIN_GREEN_2");
+    const bluePin2 = block.getFieldValue("PIN_BLUE_2");
+
     Blockly["Arduino"].variables_["RED_PIN_2"] = `int RED_PIN_2 = ${redPin2};`;
     Blockly["Arduino"].variables_["GREEN_PIN_2"] = `int GREEN_PIN_2 = ${greenPin2};`;
     Blockly["Arduino"].variables_["BLUE_PIN_2"] = `int BLUE_PIN_2 = ${bluePin2};`;
 
     Blockly["Arduino"].setupCode_["setup_led2"] = `
-  pinMode(RED_PIN_2, OUTPUT);
-  pinMode(GREEN_PIN_2, OUTPUT);
-  pinMode(BLUE_PIN_2, OUTPUT);`;
+pinMode(RED_PIN_2, OUTPUT);
+pinMode(GREEN_PIN_2, OUTPUT);
+pinMode(BLUE_PIN_2, OUTPUT);`;
 
     Blockly["Arduino"].functions_["set_led_color"] = `
 void setLedColor(RGB color, int ledNumber) {
@@ -67,7 +84,9 @@ void setLedColor(RGB color) {
 };
 
 Blockly["Arduino"]["set_color_led"] = function (block: Block) {
-  let color = rgbToColorStruct(hexToRgb(block.getFieldValue("COLOR")));
+  let hexColor = block.getFieldValue("COLOR");
+  let color = rgbToColorStruct(hexToRgb(hexColor));
+
   if (_.isEmpty(color)) {
     color = "{ 0, 0, 0 }";
   }
@@ -82,82 +101,59 @@ Blockly["Arduino"]["set_color_led"] = function (block: Block) {
 
 Blockly["Arduino"]["set_simple_color_led"] = Blockly["Arduino"]["set_color_led"];
 
-// =============== PYTHON GENERATOR =============== //
+// =============== PYTHON GENERATOR ===============
 
 Blockly["Python"]["rgb_led_setup"] = function (block: Block) {
   const redPin1 = block.getFieldValue("PIN_RED_1");
   const greenPin1 = block.getFieldValue("PIN_GREEN_1");
   const bluePin1 = block.getFieldValue("PIN_BLUE_1");
 
-  const redPin2 = block.getFieldValue("PIN_RED_2");
-  const greenPin2 = block.getFieldValue("PIN_GREEN_2");
-  const bluePin2 = block.getFieldValue("PIN_BLUE_2");
+  Blockly["Python"].imports_["machine"] = "import machine";
 
-  const numComponents = block.getFieldValue("NUMBER_OF_COMPONENTS");
+  return `
+# Initialize pins
+red_pin = machine.PWM(machine.Pin(${redPin1}))
+green_pin = machine.PWM(machine.Pin(${greenPin1}))
+blue_pin = machine.PWM(machine.Pin(${bluePin1}))
 
-  Blockly["Python"].definitions_["import_pyfirmata"] = "from pyfirmata import Arduino, util";
-
-  Blockly["Python"].variables_["red1"] = `red1 = board.digital[${redPin1}]`;
-  Blockly["Python"].variables_["green1"] = `green1 = board.digital[${greenPin1}]`;
-  Blockly["Python"].variables_["blue1"] = `blue1 = board.digital[${bluePin1}]`;
-
-  Blockly["Python"].setupCode_["setup_led1"] = `
-red1.mode = 1
-green1.mode = 1
-blue1.mode = 1`;
-
-  if (numComponents === "2") {
-    Blockly["Python"].variables_["red2"] = `red2 = board.digital[${redPin2}]`;
-    Blockly["Python"].variables_["green2"] = `green2 = board.digital[${greenPin2}]`;
-    Blockly["Python"].variables_["blue2"] = `blue2 = board.digital[${bluePin2}]`;
-
-    Blockly["Python"].setupCode_["setup_led2"] = `
-red2.mode = 1
-green2.mode = 1
-blue2.mode = 1`;
-
-    Blockly["Python"].functions_["set_led_color"] = `
-def set_led_color(color, led_number):
-    r, g, b = color
-    r = 1 if r > 127 else 0
-    g = 1 if g > 127 else 0
-    b = 1 if b > 127 else 0
-    if led_number == 1:
-        red1.write(r)
-        green1.write(g)
-        blue1.write(b)
-    else:
-        red2.write(r)
-        green2.write(g)
-        blue2.write(b)
+# Initialize the program settings
+red_pin.freq(1000)
+green_pin.freq(1000)
+blue_pin.freq(1000)
 `;
-  } else {
-    Blockly["Python"].functions_["set_led_color"] = `
-def set_led_color(color):
-    r, g, b = color
-    r = 1 if r > 127 else 0
-    g = 1 if g > 127 else 0
-    b = 1 if b > 127 else 0
-    red1.write(r)
-    green1.write(g)
-    blue1.write(b)
-`;
-  }
-
-  return "";
 };
 
 Blockly["Python"]["set_color_led"] = function (block: Block) {
-  const hex = block.getFieldValue("COLOR") || "#000000";
-  const { r, g, b } = hexToRgb(hex);
-  const colorTuple = `(${r}, ${g}, ${b})`;
+  let hexColor = block.getFieldValue("COLOR");
+  let colorStruct = hexToRgb(hexColor);
 
-  if (Blockly["Python"].variables_["red2"] !== undefined) {
-    const ledNumber = +block.getFieldValue("WHICH_COMPONENT");
-    return `set_led_color(${colorTuple}, ${ledNumber})  # Set LED ${ledNumber} color\n`;
+  let r = 0, g = 0, b = 0;
+  if (colorStruct) {
+    r = colorStruct.r ?? 0;
+    g = colorStruct.g ?? 0;
+    b = colorStruct.b ?? 0;
   }
 
-  return `set_led_color(${colorTuple})  # Set RGB LED color\n`;
+  // Invert PWM for LEDs (assuming active-low wiring)
+  const rDuty = Math.round((1 - (r / 255)) * 1023);
+  const gDuty = Math.round((1 - (g / 255)) * 1023);
+  const bDuty = Math.round((1 - (b / 255)) * 1023);
+
+  return `
+red_pin.duty(${rDuty})
+green_pin.duty(${gDuty})
+blue_pin.duty(${bDuty})
+`;
 };
 
 Blockly["Python"]["set_simple_color_led"] = Blockly["Python"]["set_color_led"];
+
+// =============== FOREVER LOOP HANDLER ===============
+
+Blockly["Python"]["controls_forever"] = function (block: Block) {
+  const statements = Blockly.Python.statementToCode(block, 'DO');
+  return `
+while True:
+${statements}
+`;
+};
