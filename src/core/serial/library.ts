339c479993e2f91969f98fbdc181ea32451ffb42
Boopalan123 <boobalanm.22ece@kongu.edu>
2025-05-01 18:16:19 +0530

    Libraries for electroblocks code

---
+++ src/core/serial/library.ts
@@ -0,0 +1,62 @@
+export const libraries = [
+    {
+      name: "FastLED",
+      version: "3.6.0",
+      url: "https://downloads.arduino.cc/libraries/github.com/FastLED/FastLED-3.6.0.zip",
+      website: "https://github.com/FastLED/FastLED",
+    },
+    {
+      name: "SoftwareSerialTX",
+      version: "0.0.33",
+      url: "https://downloads.arduino.cc/libraries/github.com/Jueff/SoftwareSerialTX-0.0.33.zip",
+      website: "https://github.com/Jueff/SoftwareSerialTX",
+    },
+    {
+      name: "SevenSegmentTM1637",
+      version: "1.1.0",
+      url: "https://downloads.arduino.cc/libraries/github.com/bremme/SevenSegmentTM1637-1.1.0.zip",
+      website: "https://github.com/bremme/arduino-tm1637",
+    },
+    {
+      name: "microWire",
+      version: "2.2.0",
+      url: "https://downloads.arduino.cc/libraries/github.com/GyverLibs/microWire-2.2.0.zip",
+      website: "https://github.com/GyverLibs/microWire",
+    },
+    {
+      name: "TinyLiquidCrystal_I2C",
+      version: "1.1.4",
+      url: "https://downloads.arduino.cc/libraries/github.com/lucas-inacio/TinyLiquidCrystal_I2C-1.1.4.zip",
+      website: "https://github.com/lucas-inacio/TinyLiquidCrystal_I2C.git",
+    },
+    {
+      name: "LiquidCrystal_I2C_Hangul",
+      version: "2.0.0",
+      url: "https://downloads.arduino.cc/libraries/github.com/junwha0511/LiquidCrystal_I2C_Hangul-2.0.0.zip",
+      website: "https://github.com/junwha0511/LiquidCrystal_I2C_Hangul",
+    },
+    {
+      name: "LED",
+      version: "1.3.0",
+      url: "https://downloads.arduino.cc/libraries/github.com/gmarty2000/LED-1.3.0.zip",
+      website: "https://github.com/gmarty2000/arduino-LED",
+    },
+    {
+      name: "LedControl",
+      version: "1.0.6",
+      url: "https://downloads.arduino.cc/libraries/github.com/wayoda/LedControl-1.0.6.zip",
+      website: "http://wayoda.github.io/LedControl/",
+    },
+    {
+      name: "Servo",
+      version: "1.2.1",
+      url: "https://downloads.arduino.cc/libraries/github.com/arduino-libraries/Servo-1.2.1.zip",
+      website: "https://www.arduino.cc/reference/en/libraries/servo/",
+    },
+    {
+      name: "Stepper",
+      version: "1.1.3",
+      url: "https://downloads.arduino.cc/libraries/github.com/arduino-libraries/Stepper-1.1.3.zip",
+      website: "https://www.arduino.cc/en/Reference/Stepper",
+    },
+  ];
--- src/core/serial/upload.ts
+++ src/core/serial/upload.ts
@@ -1,43 +1,81 @@
-import type { MicroControllerType } from "../microcontroller/microcontroller";
+import { ProgramConfig, upload, WebSerialPortPromise } from "@duinoapp/upload-multitool";
 import config from "../../env";
+import { MicroControllerType } from "../microcontroller/microcontroller";
+import { filter } from "lodash";
+import { libraries } from "./library";
 
-declare class AvrgirlArduino {
-  constructor(config: any);
-
-  flash(hex: string, call: (error) => void): void;
-}
-
-export const upload = async (
-  code: string,
-  avrgirl: AvrgirlArduino,
-  type: MicroControllerType
-) => {
-  const hexCode = await compileCode(code, type);
-
-  const enc = new TextEncoder();
-  return new Promise((res, rej) => {
-    avrgirl.flash(enc.encode(hexCode) as any, (error) => {
-      if (error) {
-        rej(error);
-      } else {
-        res("flash successful");
-      }
-    });
-  });
-};
 
 const compileCode = async (code: string, type: string): Promise<string> => {
-  const headers = new Headers();
-  headers.append("Content-Type", "text/plain");
+  const headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  const includedLibraries = code
+    .match(/#include\s+<([\w_]+)\.h>/g)
+    ?.map((match) => match.match(/<([\w_]+)\.h>/)?.[1]) || [];
 
-  const response = await fetch(
-    `${config.server_arduino_url}/upload-code/${type}`,
-    {
-      method: "POST",
-      body: code,
-      headers,
-    }
+  // Match extracted library names with metadata in library.ts
+  const libs = libraries.filter((lib) =>
+    includedLibraries.includes(lib.name)
   );
 
-  return await response.text();
+  try {
+    ///
+    var jsonString = {
+      fqbn: "arduino:avr:uno",
+      files: [
+        {
+          content: code,
+          name: "arduino/arduino.ino",
+        },
+      ],
+      flags: { verbose: false, preferLocal: false },
+      libs,
+    };
+
+    console.log(`Sending code to https://compile.duino.app/v3/compile`);
+    const response = await fetch(`https://compile.duino.app/v3/compile`, {
+      method: "POST",
+      body: JSON.stringify(jsonString),
+      headers,
+    });
+    if (!response.ok)
+      throw new Error(`Server responded with status: ${response.status}`);
+    const result = await response.json();
+    return result.hex;
+  } catch (error) {
+    console.error("Code compilation error:", error);
+    throw error;
+  }
 };
+
+export const arduinoUploader = async (
+  code: string,
+  type: MicroControllerType
+): Promise<string> => {
+  if (!WebSerialPortPromise.isSupported()) {
+    throw new Error("Web Serial API is not supported in this environment");
+  }
+  const serialport = await WebSerialPortPromise.requestPort(
+    {},
+    { baudRate: 115200 }
+  );
+  const hexCode = await compileCode(code, type);
+  const config = {
+    bin: hexCode,
+    // files: filesData,
+    // flashFreq: flashFreqData,
+    // flashMode: flashModeData,
+    speed: 115200,
+    uploadSpeed: 115200,
+    tool: type == MicroControllerType.ESP32 ? "esptool" : "avrdude",
+    cpu: "atmega328p",
+    stdout: {
+      write: (msg: string) => console.log(msg), // Properly implement the write method
+    },
+    verbose: true,
+  } as any as ProgramConfig;
+  await upload(serialport.port, config);
+
+  return "Upload successful";
+};
+
