a0fd37a162b2a87bfe0471a1325189798be37bca
Zoha Faiyaz <116448034+imnot4real@users.noreply.github.com>
2025-05-01 23:30:36 +0530

    generator for motion sensor

--- src/blocks/ultrasonic_sensor/generators.ts
+++ src/blocks/ultrasonic_sensor/generators.ts
@@ -30,3 +30,42 @@ double ultraSonicDistance() {
 
   return "";
 };
+
+Blockly.Python["ultra_sonic_sensor_motion"] = function (block: Block) {
+  return ["ultra_sonic_distance()", Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python["ultra_sonic_sensor_setup"] = function (block: Block) {
+  const echoPin = block.getFieldValue("PIN_ECHO");
+  const trigPin = block.getFieldValue("PIN_TRIG");
+
+  Blockly.Python.definitions_ = Blockly.Python.definitions_ || {};
+  Blockly.Python.setups_ = Blockly.Python.setups_ || {};
+
+  Blockly.Python.definitions_["import_gpio"] = "import RPi.GPIO as GPIO";
+  Blockly.Python.definitions_["import_time"] = "import time";
+
+  Blockly.Python.setups_["gpio_mode"] = "GPIO.setmode(GPIO.BCM)";
+  Blockly.Python.setups_["setup_echo_pin"] = `GPIO.setup(${echoPin}, GPIO.IN)`;
+  Blockly.Python.setups_["setup_trig_pin"] = `GPIO.setup(${trigPin}, GPIO.OUT)`;
+
+  Blockly.Python.definitions_["ultrasonic_function"] = `
+def ultra_sonic_distance():
+    GPIO.output(${trigPin}, GPIO.LOW)
+    time.sleep(0.000002)
+    GPIO.output(${trigPin}, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(${trigPin}, GPIO.LOW)
+
+    while GPIO.input(${echoPin}) == 0:
+        pulse_start = time.time()
+    while GPIO.input(${echoPin}) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    return round(distance, 2)
+`;
+
+  return "";
+};
