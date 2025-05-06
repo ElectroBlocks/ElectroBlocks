46dd2d083ef2830aaf60040826d7d29688bb5cdb
Boopalan123 <boobalanm.22ece@kongu.edu>
2025-05-06 18:21:07 +0530

    Libraries for electroblocks code

--- src/core/microcontroller/microcontroller.helpers.ts
+++ src/core/microcontroller/microcontroller.helpers.ts
@@ -3,22 +3,25 @@ import type { MicroController, MicroControllerBlocks } from "./microcontroller";
 const pinArrayToFieldList = (pins: string[]): [string, string][] => {
   return pins.map((pin) => [pin, pin]);
 };
-
 export const transformBoardBlockly = (
   board: MicroController
 ): MicroControllerBlocks => {
+  if (!board) {
+    throw new Error("Board object is undefined");
+  }
+
   return {
-    serial_baud_rate: board.serial_baud_rate,
+    serial_baud_rate: board.serial_baud_rate || 9600, // Default to 9600 if undefined
     type: board.type,
-    pwmPins: pinArrayToFieldList(board.pwmPins),
-    pwmNonAnalogPins: pinArrayToFieldList(board.pwmNonAnalogPins),
-    digitalPins: pinArrayToFieldList(board.digitalPins),
-    analogPins: pinArrayToFieldList(board.analonPins),
-    sckPins: pinArrayToFieldList(board.sckPins),
-    sclPins: pinArrayToFieldList(board.sclPins),
-    sdaPins: pinArrayToFieldList(board.sdaPins),
-    ssPins: pinArrayToFieldList(board.ssPins),
-    misoPins: pinArrayToFieldList(board.misoPins),
-    mosiPins: pinArrayToFieldList(board.mosiPins),
+    pwmPins: pinArrayToFieldList(board.pwmPins || []),
+    pwmNonAnalogPins: pinArrayToFieldList(board.pwmNonAnalogPins || []),
+    digitalPins: pinArrayToFieldList(board.digitalPins || []),
+    analogPins: pinArrayToFieldList(board.analonPins || []),
+    sckPins: pinArrayToFieldList(board.sckPins || []),
+    sclPins: pinArrayToFieldList(board.sclPins || []),
+    sdaPins: pinArrayToFieldList(board.sdaPins || []),
+    ssPins: pinArrayToFieldList(board.ssPins || []),
+    misoPins: pinArrayToFieldList(board.misoPins || []),
+    mosiPins: pinArrayToFieldList(board.mosiPins || []),
   };
 };
