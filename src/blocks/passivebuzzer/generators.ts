1eef15b212fbbe72d9585c8cf67012a279f712a6
Komi <uv47756@gmail.com>
2025-05-15 03:45:18 +0530

    Added passive buzzer generator for python

--- src/blocks/passivebuzzer/generators.ts
+++ src/blocks/passivebuzzer/generators.ts
@@ -13,6 +13,22 @@ Blockly["Arduino"]["passive_buzzer_note"] = function (block) {
   return `tone(${pin}, ${tone});\n`;
 };
 
+Blockly["Python"]["passive_buzzer_note"] = function (block) {
+  var tone = +block.getFieldValue("TONE");
+  var pin = block.getFieldValue("PIN");
+  Blockly["Python"].setupCode_[`buzzer_pin_${pin}`] =
+  `buzzer_pin_${pin} = board.get_pin('d:${pin}:p')`;
+
+  if (tone === 0) {
+    return `buzzer_pin_${pin}.write(0)\n`;
+  }
+  return `
+buzzer_pin_${pin}.write(${tone})  # Start tone (simulate square wave)
+time.sleep(1)
+buzzer_pin_${pin}.write(0)
+`;
+};
+
 Blockly["Arduino"]["passive_buzzer_tone"] = function (block) {
   var tone = +Blockly["Arduino"].valueToCode(
     block,
@@ -30,5 +46,28 @@ Blockly["Arduino"]["passive_buzzer_tone"] = function (block) {
   return `tone(${pin}, ${tone});\n`;
 };
 
+Blockly["Python"]["passive_buzzer_tone"] = function (block) {
+  var tone = +Blockly["Python"].valueToCode(
+    block,
+    "TONE",
+    Blockly["Python"].ORDER_ATOMIC
+  );
+  var pin = block.getFieldValue("PIN");
+  Blockly["Python"].setupCode_[`passive_buzzer_pin_${pin}`] = 
+  `passive_buzzer_pin_${pin} = board.get_pin('d:${pin}:p')\n`;
+
+  if (tone === 0) {
+    return `passive_buzzer_pin_${pin}.write(0)\n`;
+  }
+  return `
+passive_buzzer_pin_${pin}.write(${tone})
+time.sleep(1)
+passive_buzzer_pin_${pin}.write(0)
+`
+};
+
 Blockly["Arduino"]["passive_buzzer_simple"] =
   Blockly["Arduino"]["passive_buzzer_note"];
+
+Blockly["Python"]["passive_buzzer_simple"] =
+  Blockly["Python"]["passive_buzzer_note"];
