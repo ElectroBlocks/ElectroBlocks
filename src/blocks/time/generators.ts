eec4afa5abdfe473f1657d5d4ac897f318772887
Noah-Glaser <noah.glaser@docusign.com>
2025-06-04 22:07:51 -0700

    taking out semi colon

--- src/blocks/time/generators.ts
+++ src/blocks/time/generators.ts
@@ -49,7 +49,7 @@ Blockly["Python"]["delay_block"] = function (block) {
       Blockly["Arduino"].ORDER_ATOMIC
     ) || 1;
 
-  return `time.sleep(${delay}); # Wait for the given/defined seconds.\n`;
+  return `time.sleep(${delay}) # Wait for the given/defined seconds.\n`;
 };
 
 Blockly["Arduino"]["time_setup"] = function () {
