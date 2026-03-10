3329a8b90c2e3661c3be8fea35e8edb00dba4eb3
Noah-Glaser <noah.glaser@docusign.com>
2026-03-09 21:19:38 -0700

    Fixed the default langauge error

--- src/stores/settings.store.ts
+++ src/stores/settings.store.ts
@@ -4,7 +4,7 @@ import is_browser from "../helpers/is_browser";
 import { defaultSetting } from "../firebase/model";
 import _ from "lodash";
 import authStore from "./auth.store";
-import { MicroControllerType } from "../core/microcontroller/microcontroller";
+import { MicroControllerType, SUPPORTED_LANGUAGES } from "../core/microcontroller/microcontroller";
 import { getSettings } from "../firebase/db";
 let settings;
 try {
@@ -12,11 +12,17 @@ try {
     is_browser() && localStorage.getItem("settings")
       ? JSON.parse(localStorage.getItem("settings"))
       : defaultSetting;
+
+  if (!settings.language) {
+    settings.language = SUPPORTED_LANGUAGES.C;
+  }
+
 } catch (e) {
   settings = defaultSetting;
 }
 
 settings["boardType"] = settings.boardType || MicroControllerType.ARDUINO_UNO;
+settings.language = settings.language || SUPPORTED_LANGUAGES.C;
 
 const settingsStore = writable<Settings>(settings);
 
@@ -24,6 +30,9 @@ authStore.subscribe(async (auth) => {
   if (auth.isLoggedIn && is_browser()) {
     try {
       const settingsFB = await getSettings(auth.uid);
+      if (!settingsFB.language) {
+        settingsFB.language = SUPPORTED_LANGUAGES.C;
+      }
       settingsStore.set(settingsFB);
     } catch (e) {
       console.log(e, "error settings");
@@ -33,6 +42,9 @@ authStore.subscribe(async (auth) => {
 
 settingsStore.subscribe((newSettings) => {
   if (is_browser()) {
+    if (!newSettings.language) {
+      newSettings.language = SUPPORTED_LANGUAGES.C;
+    }
     localStorage.setItem("settings", JSON.stringify(newSettings));
   }
 });
